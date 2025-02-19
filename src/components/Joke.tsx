daa40f8b171edb256c170576e76d8fe2dfd73983
Aleksei Lapygin <alex.lapygin@gmail.com>
2025-02-20 00:08:25 +0100

    ui fix

--- src/components/Joke.tsx
+++ src/components/Joke.tsx
@@ -22,13 +22,13 @@ export default function Joke() {
   const { currentJoke } = useJokeContext();
   return (
     currentJoke && (
-    <div className="p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow">
+    <div className="p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow max-w-full sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl mx-auto">
       <div className="space-y-2 mb-4">
         <p className="text-lg font-medium">Q: {currentJoke.question}</p>
         <p className="text-gray-600">A: {currentJoke.answer}</p>
       </div>
 
-      <div className="flex flex-wrap gap-2">
+      <div className="flex flex-wrap justify-center gap-2">
         {currentJoke.voteStatuses.map((status) => (
           <VoteButton
             key={status.label}
